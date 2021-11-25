4d595279f610d19fd4672d1d907477d059eb90fa
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-11-25 23:57:26 +0800

    cf_25A

---
+++ Codeforces/Practice/25A_IQTest.cpp
@@ -0,0 +1,43 @@
+// Author : thisiszammy
+// Problem URL : https://codeforces.com/problemset/problem/25/A
+
+#include <iostream>
+
+int main(){
+	
+	int n;
+	
+	std::cin >> n;
+	
+	int arr[n];
+	int oddCount = 0, evenCount = 0;
+	int index = 0;
+	
+	for(int i = 0; i < n; i++){
+		
+		int x;
+		std::cin >> x;
+		
+		arr[i] = x%2;
+		
+		if(arr[i] == 1){
+			oddCount++;
+			if(oddCount>evenCount){
+				index = i;
+			}
+		}else{
+			evenCount++;
+			if(evenCount>oddCount){
+				index = i;
+			}
+		}
+		
+			
+		
+	}
+	
+	std::cout << index-1;
+	
+	
+	return 0;
+}
