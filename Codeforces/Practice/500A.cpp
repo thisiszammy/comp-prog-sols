2fc5ed75a85414597b486cf06046be97dd887bc3
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-11-27 23:01:19 +0800

    500A

---
+++ Codeforces/Practice/500A.cpp
@@ -0,0 +1,31 @@
+// Author : thisiszammy
+// Problem URL : https://codeforces.com/problemset/problem/500/A
+
+#include <iostream>
+
+int main(){
+	
+	int n, t;
+	
+	std::cin >> n >> t;
+	
+	int paths[n-1];
+	
+	for(int i = 0; i < n-1; i++){
+		std::cin >> paths[i];
+	}
+	
+	int start = 0;
+	
+	while(start < n-1){
+		if(start == t-1){
+			std::cout << "YES";
+			return 0;
+		}else{
+			start+=paths[start];
+		}
+	}
+	
+	std::cout << "NO";
+	
+}
